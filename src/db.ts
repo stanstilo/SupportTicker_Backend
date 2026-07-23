eb113a4ea9c35ef8477c5641b9cdb56be3df0cce
stanley <okstilo@gmail.com>
2026-07-23 13:03:01 +0100

    added libsql

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import Database from 'better-sqlite3'
+import Database from 'libsql'
 import { mkdirSync } from 'node:fs'
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
